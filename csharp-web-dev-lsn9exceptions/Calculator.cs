e7a32251f396b5388dbcdcf1416372a3bd553fd6
Jed Morris <jemorris110@gmail.com>
2020-06-16 08:15:46 -0700

    created class Calculator and method divide

---
+++ csharp-web-dev-lsn9exceptions/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp_web_dev_lsn9exceptions
+{
+    public class Calculator
+    {
+        static double Divide(double numerator, double denominator)
+        {
+            return numerator / denominator;
+        }
+    }
+}
